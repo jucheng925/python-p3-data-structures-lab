b1f18af8a77c6b971ff7ddc7f08d4642d25355b2
Julie C <jucheng925@gmail.com>
2023-09-16 19:11:11 -0400

    practice on data structures problem

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,28 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return [food.get("name") for food in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if food.get("heat_level") > 5]
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        print(f'{food.get("name")} ({food.get("cuisine")}) | Heat Level: {"🌶" * food.get("heat_level")}')
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
-
+    for food in spicy_foods:
+        if food.get("cuisine") == cuisine:
+            return food
+    
 def print_spiciest_foods(spicy_foods):
-    pass
+    spiciest_food = get_spiciest_foods(spicy_foods)
+    print_spicy_foods(spiciest_food)
 
 def get_average_heat_level(spicy_foods):
-    pass
+    heat_level = [food.get("heat_level") for food in spicy_foods]
+    return sum(heat_level) / len(heat_level)
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
